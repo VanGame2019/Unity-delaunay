a427cb4e4ebac7533ce5dc5c5cb76203e45d5e31
Jason <vangame2019@163.com>
2020-10-12 22:54:35 +0800

    随机小地图生成

--- demo/Assets/Unity-delaunay/van/Main_WalkMap.cs
+++ demo/Assets/Unity-delaunay/van/Main_WalkMap.cs
@@ -1,9 +1,117 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace van
 {
     public class Main_WalkMap:MonoBehaviour
     {
+        private SmallMapGenerator Generator;
+        
+        public List<Sprite> spriteList;
+        public SpriteRenderer sample;
+        
+        private void Awake()
+        {
+            Generator = new SmallMapGenerator();
+            createMap();
+        }
+
+        int w = 100;
+        int h = 100;
+        private void createMap()
+        {
+            
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    var terrain = Generator.getTerrain(i, j);
+                    var terrainObj = Generator.getTerrainObject(i, j);
+                    var location = Generator.getLocation(i, j);
+
+                    if (terrain == eTerrainBase.Grass)
+                    {
+                        var sp = Instantiate(sample);
+                        sp.sprite = spriteList[0];
+                        sp.transform.position = getPosition(i,j);
+                        sp.sortingOrder = 0;
+                    }
+
+                    switch (terrainObj)
+                    {
+                        case eTerrainObj.None:
+                            break;
+                        case eTerrainObj.Tree:
+                        {
+                            if(location!=eLocation.None)
+                                break;
+                            var sp = Instantiate(sample);
+                            sp.sprite = spriteList[5];
+                            sp.transform.position = getPosition(i,j);
+                            sp.sortingLayerName = "Object";
+                            sp.sortingOrder = -j*100;
+                        }
+                           
+                            break;
+                        case eTerrainObj.Hill:
+                        {
+                            if(location!=eLocation.None)
+                                break;
+                            var sp = Instantiate(sample);
+                            sp.sprite = spriteList[3];
+                            sp.transform.position = getPosition(i,j);
+                            sp.sortingLayerName = "Object";
+                            sp.sortingOrder = -j*100;
+                        }
+                            
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+
+                    switch (location)
+                    {
+                        case eLocation.None:
+                            break;
+                        case eLocation.City:
+                        {
+                            var sp = Instantiate(sample);
+                            sp.sprite = spriteList[1];
+                            sp.transform.position = getPosition(i,j);
+                            sp.sortingLayerName = "Object";
+                            sp.sortingOrder = -j*100+1;
+                        }
+                            break;
+                        case eLocation.Village:
+                        {
+                            var sp = Instantiate(sample);
+                            sp.sprite = spriteList[4];
+                            sp.transform.position = getPosition(i,j);
+                            sp.sortingLayerName = "Object";
+                            sp.sortingOrder = -j*100+1;
+                        }
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+        }
+
+        private Vector3 getPosition(int x, int y)
+        {
+            var xGap = 1f;
+            var yGap = 0.75f;
+            var _x = (-w / 2 + x) * xGap;
+            var _y = (-h / 2 + y) * yGap;
+            
+            if (y % 2 == 0)
+            {
+                _x += xGap / 2;
+            }
+            return new Vector3(_x,0,_y);
+        }
         
     }
 }
--- demo/Assets/Unity-delaunay/van/mapSmall/SmallMapGenerator.cs
+++ demo/Assets/Unity-delaunay/van/mapSmall/SmallMapGenerator.cs
@@ -1,7 +1,43 @@
+using UnityEngine;
+
 namespace van
 {
+    public enum eTerrainBase{Base,Grass}
+    public enum eTerrainObj{None,Tree,Hill}
+    public enum eLocation{None,City,Village}
+    
     public class SmallMapGenerator
     {
+        
+        //生成地形
+        public eTerrainBase getTerrain(int x, int y)
+        {
+            if (nextRandom < 2)
+                return eTerrainBase.Grass;
+            return eTerrainBase.Base;
+        }
+
+        //生成地形物体
+        public eTerrainObj getTerrainObject(int x, int y)
+        {
+            if(nextRandom>30)
+                return eTerrainObj.None;
+            if (nextRandom > 40)
+                return eTerrainObj.Hill;
+            return eTerrainObj.Tree;
+        }
+        
+        //生成具体的
+        public eLocation getLocation(int x, int y)
+        {
+            if (nextRandom > 2)
+                return eLocation.None;
+            if (nextRandom > 20)
+                return eLocation.Village;
+            return eLocation.City;
+        }
+
+        private float nextRandom=>Random.Range(0, 100f);
         
     }
 }
